d5f9b206041ffb8b1d63ff67295993b541ddf5db
pacifi <pacifi.bnr@gmail.com>
2026-05-11 18:50:34 -0500

    change java version 21

--- src/test/java/pe/edu/upeu/sales_api/SalesApiApplicationTests.java
+++ src/test/java/pe/edu/upeu/sales_api/SalesApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SalesApiApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
